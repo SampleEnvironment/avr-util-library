63d2514b3a631c697a02a7f97b8ff27b6eb258d5
Wegmann Peter <peter.wegmann@helmholtz-berlin.de>
2021-09-29 11:49:53 +0200

    added xbee infoline strings to ilm module

--- printInfo_strings.h
+++ printInfo_strings.h
@@ -57,25 +57,25 @@
 #endif
 
 #ifdef  ILM_MODULE
-#define XBEE_AI_MESSAGE    "" // API command "AI" => Reads possible errors with the last association request, result 0: everything is o.k.
-#define XBEE_REASSOCIATE_   ""
-#define XBEE_REASSOCIATE__  ""
-#define XBEE_REASSOCIATE___ ""
-#define XBEE_DEVICE_ASSOCIATED ""
+#define XBEE_AI_MESSAGE    "AImess" // API command "AI" => Reads possible errors with the last association request, result 0: everything is o.k.
+#define XBEE_REASSOCIATE_   "con.  "
+#define XBEE_REASSOCIATE__  "con . "
+#define XBEE_REASSOCIATE___ "con  ."
+#define XBEE_DEVICE_ASSOCIATED "DevAss"
 #define XBEE_PRINT_NULL    ""
-#define XBEE_FAILED ""
-#define XBEE_NETWORK_ERROR      ""
-#define XBEE_NETWORK_ERROR_ADDR ""
-#define XBEE_SENDING            ""
-#define XBEE_SENDING_ERROR      ""
-#define XBEE_SENDING_OK         ""
-#define XBEE_CHECK_NETWORK      ""
-#define XBEE_NO_NETWORK		    ""
-#define XBEE_RECONNECTING       ""
-#define XBEE_SENDING_MESSAGE    ""
-#define XBEE_PING				""
-#define XBEE_PING_OK            ""
-#define XBEE_NO_SERV			""
+#define XBEE_FAILED "failed"
+#define XBEE_NETWORK_ERROR      "NW err"
+#define XBEE_NETWORK_ERROR_ADDR "AD err"
+#define XBEE_SENDING            "SendMs"
+#define XBEE_SENDING_ERROR      "Snd er"
+#define XBEE_SENDING_OK         "SendOK"
+#define XBEE_CHECK_NETWORK      "CkNetw"
+#define XBEE_NO_NETWORK		    "NoNetw "
+#define XBEE_RECONNECTING       "recon."
+#define XBEE_SENDING_MESSAGE    "SendMs"
+#define XBEE_PING				"Ping.."
+#define XBEE_PING_OK            "PingOK"
+#define XBEE_NO_SERV			"no Serv"
 #endif
 
 
